252a8e9c6d36765a441aae313ef3440cf4e95494
clemado1 <clemado1@gmail.com>
2021-01-29 00:29:07 +0900

    Jwt Config Class

--- src/main/java/com/mac/doc/SecurityConfig.java
+++ src/main/java/com/mac/doc/SecurityConfig.java
@@ -1,50 +1,73 @@
 package com.mac.doc;
 
-import com.mac.doc.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.mac.doc.jwt.JwtAccessDeniedHandler;
+import com.mac.doc.jwt.JwtAuthenficationEntryPoint;
+import com.mac.doc.jwt.JwtSecurityConfig;
+import com.mac.doc.jwt.TokenProvider;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenficationEntryPoint jwtAuthenficationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
-    private final UserService userService;
+    public SecurityConfig(TokenProvider tokenProvider,
+                          JwtAuthenficationEntryPoint jwtAuthenficationEntryPoint,
+                          JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenficationEntryPoint = jwtAuthenficationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
 
-    public SecurityConfig(UserService userService) {
-        this.userService = userService;
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/lib/**");
+        web
+                .ignoring()
+                .antMatchers("/favicon.ico", "/css/**", "/js/**", "/img/**", "/lib/**");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/admin/**").hasAuthority("ADMIN")
-                .antMatchers("/doc/**").hasAuthority("MEMBER")
+        http
+                .csrf().disable()
+
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenficationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                .and() // h2
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**/**").authenticated()
                 .antMatchers("/**").permitAll()
+
                 .and()
-                .formLogin()
-                .defaultSuccessUrl("/user/login/result")
-                .permitAll()
-                .and()
-                .logout()
-                .logoutSuccessUrl("/user/logout/result")
-                .invalidateHttpSession(true)
-                .and()
-                .exceptionHandling().accessDeniedPage("/user/denied");
+                .apply(new JwtSecurityConfig(tokenProvider));
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
-    }
 }
---
+++ src/main/java/com/mac/doc/jwt/JwtFilter.java
@@ -0,0 +1,50 @@
+package com.mac.doc.jwt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class JwtFilter extends GenericFilterBean {
+    private static final String AUTHORITIES_HEADER = "Authorization";
+    private final Logger logger = LoggerFactory.getLogger(JwtFilter.class);
+    private TokenProvider tokenProvider;
+
+    public JwtFilter(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String jwt = resolveToken(httpServletRequest);
+        String requestURI = httpServletRequest.getRequestURI();
+
+        if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+            Authentication authentication = tokenProvider.getAuthentication(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            logger.debug("Security Context '{}', uri: {}", authentication.getName(), requestURI);
+        } else {
+            logger.debug("Not Valid JWT Token, uri: {}", requestURI);
+        }
+
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORITIES_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
