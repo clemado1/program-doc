a018df8bbc6880e543b6cc2af78f53ecaefbefd6
clemado1 <clemado1@gmail.com>
2021-09-04 01:19:02 +0900

    mockito stubbing

--- src/test/java/com/mac/doc/service/FunctionServiceTest.java
+++ src/test/java/com/mac/doc/service/FunctionServiceTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,11 +27,34 @@ class FunctionServiceTest {
     }
 
     @Test
-    void findFunction() {
+    void findFunction1() {
         FunctionDto dto = new FunctionDto();
         dto.setFunctionCd("COM1");
         dto.setFunctionNm("공통");
 
-        when(functionService.findFunction("COM1")).thenReturn(dto);
+        when(functionService.findFunction(any())).thenReturn(dto);
+
+        Assertions.assertEquals("공통", functionService.findFunction("COM1").getFunctionNm());
+        Assertions.assertEquals("공통", functionService.findFunction("COM2").getFunctionNm());
+
+        doThrow(new IllegalArgumentException()).when(functionService).findFunctions();
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> functionService.findFunctions());
+    }
+
+    @Test
+    void findFunction2() {
+        FunctionDto dto = new FunctionDto();
+        dto.setFunctionCd("COM1");
+        dto.setFunctionNm("공통");
+
+        when(functionService.findFunction(any()))
+                .thenReturn(dto)
+                .thenThrow(new RuntimeException())
+                .thenReturn(null);
+
+        Assertions.assertEquals("공통", functionService.findFunction("COM1").getFunctionNm());
+        Assertions.assertThrows(RuntimeException.class, () -> functionService.findFunction("COM2"));
+        Assertions.assertNull(functionService.findFunction("COM3"));
     }
 }
